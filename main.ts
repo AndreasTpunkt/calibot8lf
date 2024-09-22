46ea2bf8a41209b52b888daf73bb7ac31fed3c90
AndreasTpunkt <103647180+AndreasTpunkt@users.noreply.github.com>
2024-09-22 12:26:37 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,30 @@
+let V_langsam = 10
+let V_normal = 30
+basic.forever(function () {
+    if (calliBot2E.distance(C2eEinheit.cm) > 3) {
+        calliBot2E.rgbLed(C2eRgbLed.LV, 16, 16, 16)
+        calliBot2E.rgbLed(C2eRgbLed.RV, 16, 16, 16)
+        calliBot2E.rgbLed(C2eRgbLed.LH, 16, 0, 0)
+        calliBot2E.rgbLed(C2eRgbLed.RH, 16, 0, 0)
+        if (calliBot2E.readLineSensor(C2eSensor.links, C2eSensorStatus.dunkel) && calliBot2E.readLineSensor(C2eSensor.rechts, C2eSensorStatus.dunkel)) {
+            calliBot2E.motor(C2eMotor.beide, C2eDir.vorwärts, V_normal)
+        } else if (calliBot2E.readLineSensor(C2eSensor.links, C2eSensorStatus.dunkel) && calliBot2E.readLineSensor(C2eSensor.rechts, C2eSensorStatus.hell)) {
+            calliBot2E.motor(C2eMotor.links, C2eDir.vorwärts, V_langsam)
+            calliBot2E.motor(C2eMotor.rechts, C2eDir.vorwärts, V_normal)
+        } else if (calliBot2E.readLineSensor(C2eSensor.links, C2eSensorStatus.hell) && calliBot2E.readLineSensor(C2eSensor.rechts, C2eSensorStatus.dunkel)) {
+            calliBot2E.motor(C2eMotor.links, C2eDir.vorwärts, V_normal)
+            calliBot2E.motor(C2eMotor.rechts, C2eDir.vorwärts, V_langsam)
+        } else {
+            calliBot2E.motorStop(C2eMotor.beide, C2eStop.Bremsen)
+            calliBot2E.motor(C2eMotor.links, C2eDir.vorwärts, V_normal)
+            calliBot2E.motor(C2eMotor.rechts, C2eDir.rückwärts, V_normal)
+        }
+    } else {
+        calliBot2E.motorStop(C2eMotor.beide, C2eStop.Bremsen)
+        calliBot2E.rgbLed(C2eRgbLed.LV, 0, 0, 16)
+        calliBot2E.rgbLed(C2eRgbLed.RV, 0, 0, 16)
+        calliBot2E.rgbLed(C2eRgbLed.LH, 0, 16, 16)
+        calliBot2E.rgbLed(C2eRgbLed.RH, 0, 16, 16)
+        calliBot2E.led(C2eMotor.beide, C2eState.aus)
+    }
+})
